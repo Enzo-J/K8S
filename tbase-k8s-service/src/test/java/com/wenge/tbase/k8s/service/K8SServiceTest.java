25c91acafa383fb79230268d01351285bb93ab35
Donson <18826131280@139.com>
2020-12-02 19:42:27 +0800

    增加测试用例

--- tbase-k8s-service/src/test/java/com/wenge/tbase/k8s/service/K8SServiceTest.java
+++ tbase-k8s-service/src/test/java/com/wenge/tbase/k8s/service/K8SServiceTest.java
@@ -1,30 +1,27 @@
 package com.wenge.tbase.k8s.service;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class K8SServiceTest {
-//    @Resource
-//    private K8SService k8SService;
-//    @Test
-//    public void getAllNameSpace() {
-//        System.out.println(k8SService.getAllNameSpace());
-//    }
-//
-//    @Test
-//    public void getMachineInfo(){
-//        k8SService.getNodeInfo();
-//    }
-//
-//    @Test
-//    public void getConfigMap(){
-//        k8SService.findConfigMapDetail("jenkins","components");
-//    }
-//}
+@SpringBootTest
+public class K8SServiceTest {
+    @Resource
+    private K8SService k8SService;
+
+    @org.junit.jupiter.api.Test
+    public void getAllNameSpace() {
+        System.out.println(k8SService.getAllNameSpace());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getMachineInfo(){
+        k8SService.getNodeInfo();
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getConfigMap(){
+        k8SService.findConfigMapDetail("jenkins","components");
+    }
+}
