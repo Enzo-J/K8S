dad721664ce5dd2b1d033efec34426ad723f6e49
sunlyhm <sunlyhm1993>
2020-12-02 16:50:54 +0800

    微服务模块

--- tbase-commons/src/main/java/com/wenge/tbase/commons/result/ResultCode.java
+++ tbase-commons/src/main/java/com/wenge/tbase/commons/result/ResultCode.java
@@ -13,13 +13,13 @@ public enum ResultCode {
 
     SUCCESS(200, "操作成功"),
 
-    FAILED(400, "响应失败"),
-    
+    SYSYTM_CLIENT_ERROR(400,"客户端请求中的语法错误"),
+
     NO_PERMISSION(403, "没有权限"),
-	
+
     NOT_FIND_RESOURCE(404, "无法找到资源"),
-	
-	SYSTEM_ERROR(500, "服务器内部错误"),
+
+    SYSTEM_ERROR(500, "服务器内部错误"),
 
     VALIDATE_FAILED(1002, "参数校验失败"),
 
