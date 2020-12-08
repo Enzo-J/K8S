f5382affe11b830d2de89deaa7c83ed276c0703c
wangxingpeng <1797958963@qq.com>
2020-12-08 14:51:43 +0800

    增加流水线阶段代码
    Signed-off-by: wangxingpeng <1797958963@qq.com>

---
+++ tbase-cicd-service/src/main/java/com/wenge/tbase/cicd/service/impl/CicdPipelineStageServiceImpl.java
@@ -0,0 +1,20 @@
+package com.wenge.tbase.cicd.service.impl;
+
+import com.wenge.tbase.cicd.entity.CicdPipelineStage;
+import com.wenge.tbase.cicd.mapper.CicdPipelineStageMapper;
+import com.wenge.tbase.cicd.service.CicdPipelineStageService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Wang XingPeng
+ * @since 2020-12-02
+ */
+@Service
+public class CicdPipelineStageServiceImpl extends ServiceImpl<CicdPipelineStageMapper, CicdPipelineStage> implements CicdPipelineStageService {
+
+}
