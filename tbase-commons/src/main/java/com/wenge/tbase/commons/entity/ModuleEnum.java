c927da4555380f1313394000017c051b20a6b659
sunlyhm <sunlyhm1993>
2020-12-14 11:40:41 +0800

    数据库模板

--- tbase-commons/src/main/java/com/wenge/tbase/commons/entity/ModuleEnum.java
+++ tbase-commons/src/main/java/com/wenge/tbase/commons/entity/ModuleEnum.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 public enum ModuleEnum {
     MODULE_CICD("/tbase-cicd-service", "cicd","172.16.0.6:31372/am_cicd"),
-    MODULE_K8S("/tbase-k8s-service", "k8s","172.16.0.6:31372/am_k8s");
+    MODULE_K8S("/tbase-k8s-service", "k8s","172.16.0.6:31372/am_k8s"),
+    MODULE_COMMONS("/tbase-commons", "commons","172.16.0.6:31372/app-manage-platform"),
+    MODULE_MICRO("/tbase-microservice-service", "nacos","172.16.0.6:31372/am_microservice");
 
     private String modulePath;
 
