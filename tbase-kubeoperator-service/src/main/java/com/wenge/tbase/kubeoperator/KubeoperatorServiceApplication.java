f2379e04db32cea6c2b55c88e10a6143483363e4
贾超 <chao.jia@wengegroup.com>
2020-12-25 16:09:26 +0800

    add nacos

--- tbase-kubeoperator-service/src/main/java/com/wenge/tbase/kubeoperator/KubeoperatorServiceApplication.java
+++ tbase-kubeoperator-service/src/main/java/com/wenge/tbase/kubeoperator/KubeoperatorServiceApplication.java
@@ -2,11 +2,12 @@ package com.wenge.tbase.kubeoperator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients
 public class KubeoperatorServiceApplication {
 
 	public static void main(String[] args) {
